5ec0d0e746a838f168c2f0506ceabc8e1afa36d6
Dhwanil Ditani <ditanidhwanil@gmail.com>
2020-04-18 06:59:23 +0530

    added Passenger Input List

--- search.js
+++ search.js
@@ -1,3 +1,22 @@
+function InsertPassenger(index) {
+    var c = "passenger" + index;
+    var div = document.createElement("div");
+    div.className = c;
+    $("div.passengers").append(div);
+    var first_name = document.createElement("input");
+    first_name.type = "text";
+    first_name.name = "first_name";
+    first_name.className = c;
+    var last_name = document.createElement("input");
+    last_name.type = "text";
+    last_name.name = "last_name";
+    last_name.className = c;
+    var Phone_No = document.createElement("input");
+    Phone_No.type = "text";
+    Phone_No.name = "Phone_No";
+    Phone_No.className = c;
+    $("div." + c).append("first_name: ", first_name, "last_name: ", last_name, "Phone_No: ", Phone_No);
+}
 $("document").ready(function() {
     $("#return_date").prop('disabled', true);
     $('input[type="radio"]').change(function() {
@@ -29,9 +48,9 @@ $("document").ready(function() {
 
     });
     $("#submit_button").click(function () {
-        console.log($("input[name='trip_type']:checked").val());
-        console.log($("input[name='depart_date']").val());
-        console.log($("input[name='return_date']").val());
+        // console.log($("input[name='trip_type']:checked").val());
+        // console.log($("input[name='depart_date']").val());
+        // console.log($("input[name='return_date']").val());
         $.post("search.php",
         {
             depart_date: $("input[name='depart_date']").val(),
@@ -41,7 +60,42 @@ $("document").ready(function() {
             return_date: $("input[name='return_date']").val()
         },
         function (data) {
+            $("div.result").html("");
+            $("div.passengers").html("");
             $("div.result").html(data);
+            var trip_type = $("input[name='trip_type']:checked").val();
+            $("input[name='flight_go']").click(function () {
+                if (trip_type == "return") {
+                    if($("input[name='flight_ret']:checked").length > 0) {
+                        var n_passengers = $("select[name='passengers']").val();
+                        for (var i=0; i<n_passengers; i++) {
+                            InsertPassenger(String(i));
+                        }
+                    }
+                }
+                else {
+                    var n_passengers = $("select[name='passengers']").val();
+                    for (var i=0; i<n_passengers; i++) {
+                        InsertPassenger(String(i));
+                    }
+                    // console.log($("input[name='flight_go']:checked").length);
+                }
+            });
+
+            $("input[name='flight_ret']").click(function () {
+                if($("input[name='flight_go']:checked").length > 0) {
+                    var n_passengers = $("select[name='passengers']").val();
+                    for (var i=0; i<n_passengers; i++) {
+                        InsertPassenger(String(i));
+                    }
+                }
+            });
+            // var n_passengers = $("select[name='passengers']").val();
+            // for (var i=0; i<n_passengers; i++) {
+            //     InsertPassenger(String(i));
+            // }
+            // console.log($("input[name='flight_go']:checked").length);
+
         });
     });
 });
